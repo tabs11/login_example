9fb7b4dbe58b77e673f286cd6852a79e5cdb770e
Nuno <nunomartins_11@hotmail.com>
2019-05-15 15:58:47 +0100

    Update application.py

--- application.py
+++ application.py
@@ -38,7 +38,7 @@ USERS = { # dictionary (username, User)
 
 # application base
 application = Flask(__name__)
-application.secret_key = str(uuid.uuid1())
+application.secret_key = 'bla'
 
 application.config['CMDB_FOLDER'] = 'CMDB_templates/'
 
