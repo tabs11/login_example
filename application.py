344b877459bcb69d98aecc848cc92ed93f6869ec
Nuno <nunomartins_11@hotmail.com>
2019-05-16 12:01:35 +0100

    Update application.py

--- application.py
+++ application.py
@@ -184,7 +184,7 @@ def upload():
 		if file and allowed_file(file.filename):
 			# Make the filename safe, remove unsupported chars
 			filename = secure_filename(file.filename)
-            os.makedirs(UPLOAD_FOLDER)
+            		os.makedirs(UPLOAD_FOLDER)
 			# Move the file form the temporal folder to the upload
 			file.save(os.path.join(UPLOAD_FOLDER, filename))
 		else:
