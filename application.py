cf0f0db6ea94a10bb42165c9372a76059359ef7a
Nuno <nunomartins_11@hotmail.com>
2019-09-19 10:27:23 +0100

    Update application.py

--- application.py
+++ application.py
@@ -404,7 +404,7 @@ def load_user(userid):
 
 if __name__ == '__main__':
 	application.run(
-		host='0.0.0.0', 
-		port=3000, 
-		threaded=True
+		#host='0.0.0.0', 
+		#port=3000, 
+		#threaded=True
 	)
