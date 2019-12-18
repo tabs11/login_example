f6836ea0d002a451e944ce502dc2256d2174c81e
Nuno <nunomartins_11@hotmail.com>
2019-12-18 17:37:06 +0000

    Update process_data.py

--- process_data.py
+++ process_data.py
@@ -380,7 +380,7 @@ def process_file(path,company,report,history):
 				cis_list.append(prod_missing_final)
 				cis_list.append(wrong_Tiers)
 				issues_names=['Wrong product Name','Wrong Tiers']
-				print('PRODUCT CATALOG ISSUES:','-'*len('PRODUCT CATALOG ISSUES:'),issues,'',sep='\n',file=open(report +'issues.txt','a',encoding='utf8'))               
+				print('PRODUCT CATALOG ISSUES:','-'*len('PRODUCT CATALOG ISSUES:'),wrong_catalogue,'',sep='\n',file=open(report +'issues.txt','a',encoding='utf8'))               
 				#cis[0].to_csv(report + company +'cis_to_add_'+'.csv')			
 			else:
 				None				
